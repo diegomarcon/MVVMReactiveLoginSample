9552f00bb5cedae25749ee3f0382ebf73dce2667
Diego Marcon <diego.marcon@corp.globo.com>
2016-07-18 09:47:10 -0300

    Add login view controller basic ui

---
+++ MVVMReactiveLoginSample/Controller/LoginViewController.swift
@@ -0,0 +1,21 @@
+//
+//  LoginViewController.swift
+//  MVVMReactiveLoginSample
+//
+//  Created by Diego Marcon on 7/18/16.
+//  Copyright © 2016 marcon. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var login: UIButton!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
