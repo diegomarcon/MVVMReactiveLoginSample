b099f04350d820f71fb21a907ba7b37e6a67f71d
Diego Marcon <diego.marcon@corp.globo.com>
2016-07-18 08:54:03 -0300

    Add xcode project

---
+++ MVVMReactiveLoginSample/Controller/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MVVMReactiveLoginSample
+//
+//  Created by Diego Marcon on 7/18/16.
+//  Copyright © 2016 marcon. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
